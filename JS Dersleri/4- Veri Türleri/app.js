32fe3c1c2a966926c699b3e1ad2ef737e003abf6
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-06 14:40:53 +0300

    Veri Türleri

---
+++ JS Dersleri/4- Veri Türleri/app.js
@@ -0,0 +1,11 @@
+// string veri türü: metinsel veriler için kullanılır. Çift tırnak, tek tırnak veya backtick ile tanımlanabilir.
+
+let name = "Ali Kaan Koç";
+console.log(typeof name);
+
+// number veri türü: sayısal veriler için kullanılır. Tamsayı ve ondalıklı sayılar için kullanılır.
+
+let age = 20;
+console.log(typeof age);
+let price = 19.99;
+console.log(typeof price);
