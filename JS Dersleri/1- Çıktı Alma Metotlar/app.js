959017a448e66dbc324dd6ff53a309c5886451aa
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-06 11:29:54 +0300

    JS Çıktı Alma Metotlar

---
+++ JS Dersleri/1- Çıktı Alma Metotlar/app.js
@@ -0,0 +1,23 @@
+// document.write ile çıktı alma
+document.write("Hello World <br>");
+document.write("Ali Kaan Koç <br>");
+document.write("JavaScript Öğreniyorum");
+
+// console.log ile çıktı alma
+console.log("Hello World");
+console.log("Ali Kaan Koç");
+console.log({name: "Ali Kaan Koç", job: "Student"});
+
+console.log(window);
+console.log(window.location.href)
+
+// console.clear();  Konsolu Temizler
+
+//alert ile çıktı alma
+alert("Hello World");
+alert("Ali Kaan Koç");
+
+let a = 12;
+let b=  13;
+
+alert("İki Sayının Toplamı: " + (a+b));
