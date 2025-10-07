b5713ce0f2d451774c5789085a97f64226f1882a
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-07 14:51:48 +0300

    Confirm

--- JS Dersleri/5- Diyalog Kutuları/app.js
+++ JS Dersleri/5- Diyalog Kutuları/app.js
@@ -9,3 +9,8 @@ let name = prompt("Lütfen adınızı giriniz:");
 let age = prompt("Lütfen yaşınızı giriniz:");
 console.log("Adınız:" + name);
 console.log("Yaşınız:" + age);
+
+// confirm: Kullanıcıdan onay almak için kullanılır. Kullanıcı "Tamam" butonuna basarsa true, "İptal" butonuna basarsa false döner.
+
+let onay= confirm("Bu işlemi onaylıyor musunuz ?");
+console.log(onay);
