85aed2be254951a832058ffc9a82fa9c15d4dd75
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-07 14:49:51 +0300

    Alert - Prompt yapısı

--- JS Dersleri/5- Diyalog Kutuları/app.js
+++ JS Dersleri/5- Diyalog Kutuları/app.js
@@ -2,3 +2,10 @@
 // alert: Kullaniciya bilgi vermek icin kullanilir.
 
 alert("İşleminize devam etmek için kaydediniz !");
+
+// prompt: Kullanıcıdan bilgi almak için kullanılır. Kullanıcıdan alınan bilgi string veri türünde döner.
+
+let name = prompt("Lütfen adınızı giriniz:");
+let age = prompt("Lütfen yaşınızı giriniz:");
+console.log("Adınız:" + name);
+console.log("Yaşınız:" + age);
