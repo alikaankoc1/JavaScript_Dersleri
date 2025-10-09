ef1b7332af9334698b78f523773fe84dc5cc1625
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-09 15:32:12 +0300

    Update app.js

--- JS Dersleri/9- Gas Station Application/app.js
+++ JS Dersleri/9- Gas Station Application/app.js
@@ -6,3 +6,19 @@ const newLine = "\r\n";
 
 const yakitMetni = "1-Dizle" + newLine + "2-Benzin" + newLine + "3-LPG" + newLine + "Yakıt Türü Seçiniz:";
 
+let yakitTipi = prompt(yakitMetni);
+let litre = Number(prompt("Almak İstediğiniz Litre Tutarını Giriniz:"));
+let bakiye = Number(prompt("Lütfen Bakiyenizi Giriniz:"));
+
+if(yakitTipi == "1"){
+    // dizel
+}
+else if(yakitTipi == "2"){
+    // benzin
+}
+else if(yakitTipi == "3"){
+    // lpg
+}
+else{
+    console.log("Hatalı Yakıt Türü Girildi");
+}
