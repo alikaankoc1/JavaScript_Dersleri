dc9cfe1963288d701c7f1425cf64e357836f763c
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-09 14:06:27 +0300

    if-else example

--- JS Dersleri/7- Koşul Yapıları Example/app.js
+++ JS Dersleri/7- Koşul Yapıları Example/app.js
@@ -1,3 +1,12 @@
 let vize1 = Number(prompt("1. Vize Notunuzu Giriniz:"));
 let vize2 = Number(prompt("2. Vize Notunuzu Giriniz:"));
 let final = Number(prompt("Final Notunuzu Giriniz:"));
+
+let avarege = (vize1*0.3) + (vize2*0.3) + (final*0.4);
+
+if(avarege >= 50 && final >= 50){
+    console.log("Dersten Geçtiniz. Notunuz:" + avarege);
+}
+else{
+    console.log("Dersten Kaldınız. Notunuz:" + avarege);
+}
