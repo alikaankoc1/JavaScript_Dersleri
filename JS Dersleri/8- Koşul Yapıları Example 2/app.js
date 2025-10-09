ce5a63e4407d0c528e945b6cebc6e1be9c872b53
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-09 14:56:35 +0300

    Function example

---
+++ JS Dersleri/8- Koşul Yapıları Example 2/app.js
@@ -0,0 +1,10 @@
+let name = prompt("Lütfen adınızı giriniz:");
+let tckn = Number(prompt("Lütfen TC Kimlik Numaranızı Giriniz."));
+let phoneNumber = Number(prompt("Lütfen Telefon Numaranızı Giriniz:"));
+
+if(name = "" && tckn === 11 && phoneNumber === 10){
+    console.log("Kayıt Başarılı");
+}
+else{
+    console.log("Kayıt Başarısız");
+}
