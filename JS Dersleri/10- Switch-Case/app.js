6f5aede54cbd6e6487893f8da3648c5b06cfab39
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-09 21:21:57 +0300

    Switch - Case

---
+++ JS Dersleri/10- Switch-Case/app.js
@@ -0,0 +1,27 @@
+let number = prompt("Lütfen 1-6 arasında bir sayı giriniz:");
+
+switch(number)
+{
+    case "1":
+        console.log("Girilen sayı 1'dir.");
+
+        break;
+    case "2":
+       console.log("Girilen sayı 2'dir.");
+        break;
+    case "3":
+        console.log("Girilen sayı 3'dir.");
+        break;
+    case "4":
+        console.log("Girilen sayı 4'dir.");
+        break;
+    case "5":
+        console.log("Girilen sayı 5'dir.");
+        break;
+    case "6":
+        console.log("Girilen sayı 6'dır.");
+        break;
+    default:
+        console.log("Girilen sayı 1-6 arasında değildir.");
+        break; 
+}
