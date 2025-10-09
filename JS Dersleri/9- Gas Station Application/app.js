f1eecd685c1e99c5a6f78bf3b2946c6b4e5a7b11
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-09 15:27:19 +0300

    Gas Station Aplication

---
+++ JS Dersleri/9- Gas Station Application/app.js
@@ -0,0 +1,8 @@
+let dizel = 24.53;
+let benzin = 22.25;
+let lgp = 11.1;
+
+const newLine = "\r\n";
+
+const yakitMetni = "1-Dizle" + newLine + "2-Benzin" + newLine + "3-LPG" + newLine + "Yakıt Türü Seçiniz:";
+
