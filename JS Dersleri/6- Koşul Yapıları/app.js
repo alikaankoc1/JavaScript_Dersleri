18ea19b2b9e43fa31ba7766d51d1fc23fa7039ee
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-09 13:54:16 +0300

    if-else

---
+++ JS Dersleri/6- Koşul Yapıları/app.js
@@ -0,0 +1,19 @@
+// Koşul Yapıları
+
+let note = 65;
+
+if(note >= 50){
+    console.log("Dersten Geçtiniz:" + note);
+}
+else{
+    console.log("Dersten Kaldınız:" + note);
+}
+
+let age = Number(prompt("Lütfen yaşınızı giriniz:"));
+
+if(age >= 18){
+    alert("Ehliyet Alabilirsiniz.");
+}
+else{
+    alert("Ehliyet Alamazsınız");
+}
