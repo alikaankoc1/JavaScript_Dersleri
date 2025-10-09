5361abcae2515d9bb9a2e6f86844a02296fe4b4a
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-09 14:25:47 +0300

    if -else if- else example

--- JS Dersleri/7- Koşul Yapıları Example/app.js
+++ JS Dersleri/7- Koşul Yapıları Example/app.js
@@ -1,12 +1,33 @@
+// let vize1 = Number(prompt("1. Vize Notunuzu Giriniz:"));
+// let vize2 = Number(prompt("2. Vize Notunuzu Giriniz:"));
+// let final = Number(prompt("Final Notunuzu Giriniz:"));
+
+// let avarege = (vize1*0.3) + (vize2*0.3) + (final*0.4);
+
+// if(avarege >= 50 && final >= 50){
+//     console.log("Dersten Geçtiniz. Notunuz:" + avarege);
+// }
+// else{
+//     console.log("Dersten Kaldınız. Notunuz:" + avarege);
+// }
+
+//----------------------------------------------------------------------------------------
+
 let vize1 = Number(prompt("1. Vize Notunuzu Giriniz:"));
 let vize2 = Number(prompt("2. Vize Notunuzu Giriniz:"));
 let final = Number(prompt("Final Notunuzu Giriniz:"));
 
 let avarege = (vize1*0.3) + (vize2*0.3) + (final*0.4);
 
-if(avarege >= 50 && final >= 50){
-    console.log("Dersten Geçtiniz. Notunuz:" + avarege);
+if(avarege >= 81 && avarege <= 100){
+    console.log("Matematik 2 Ders AA ile Geçtiniz. Notunuz: " + avarege);
+}
+else if(avarege >= 61 && avarege <= 80){
+    console.log("Matematik 2 Ders BB ile Geçtiniz. Notunuz: "  + avarege );
+}
+else if(avarege >= 40 && avarege <= 60){
+    console.log("Matematik 2 Ders CC ile Geçtiniz. Notunuz: " + avarege);
 }
 else{
-    console.log("Dersten Kaldınız. Notunuz:" + avarege);
+    console.log("Matematik 2 Dersinden Kaldınız. Notunuz: " + avarege);
 }
