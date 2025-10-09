4b2b0bec3014d336917dccc344b125bf546efd9c
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-09 15:11:14 +0300

    Update app.js

--- JS Dersleri/8- Koşul Yapıları Example 2/app.js
+++ JS Dersleri/8- Koşul Yapıları Example 2/app.js
@@ -1,6 +1,6 @@
 let name = prompt("Lütfen adınızı giriniz:");
-let tckn = Number(prompt("Lütfen TC Kimlik Numaranızı Giriniz."));
-let phoneNumber = Number(prompt("Lütfen Telefon Numaranızı Giriniz:"));
+let tckn = prompt("Lütfen TC Kimlik Numaranızı Giriniz.");
+// let phoneNumber = Number(prompt("Lütfen Telefon Numaranızı Giriniz:"));
 
 // if(name = "" && tckn === 11 && phoneNumber === 10){
 //     console.log("Kayıt Başarılı");
@@ -9,23 +9,46 @@ let phoneNumber = Number(prompt("Lütfen Telefon Numaranızı Giriniz:"));
 //     console.log("Kayıt Başarısız");
 // }
 
-function check(name, tckn, phoneNumber){
-if(name !== ""){
-    if( tckn.lenght === 11){
-        if(phoneNumber.lenght === 10){
-            console.log("Kayıt Başarılı");
-        }
-        else{
-            console.log("Hatalı Telefon Numarası");
-        }
+// function check(name, tckn, phoneNumber){
+// if(name !== ""){
+//     if( tckn.lenght === 11){
+//         if(phoneNumber.lenght === 10){
+//             console.log("Kayıt Başarılı");
+//         }
+//         else{
+//             console.log("Hatalı Telefon Numarası");
+//         }
+//     }
+//     else{
+//         console.log("Hatalı TC Kimlik Numarası")
+//     }
+// }
+// else{
+// console.log("Hatalı Eksik İsim Girişi")
+// }
+// }
+
+// check(name, tckn, phoneNumber);
+
+
+function check(name, tckn) {
+    if(name == "") {
+        console.log("Hatalı Eksik İsim Girişi");
+        return;
     }
-    else{
-        console.log("Hatalı TC Kimlik Numarası")
+    // TC kimlik numarasının 11 haneli olmasını kontrol et
+    if(tckn.length !== 11 || isNaN(tckn)) {
+        console.log("Hatalı TC Kimlik Numarası");
+        return;
     }
-}
-else{
-console.log("Hatalı Eksik İsim Girişi")
-}
+
+    console.log("Kayıt Başarılı");
 }
 
-check(name, tckn, phoneNumber);
+check(name, tckn);
+
+
+
+
+
+
