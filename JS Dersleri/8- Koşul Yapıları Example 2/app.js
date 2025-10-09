16e5644f274e816215780802dce492755725ba15
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-09 15:04:25 +0300

    function example 2

--- JS Dersleri/8- Koşul Yapıları Example 2/app.js
+++ JS Dersleri/8- Koşul Yapıları Example 2/app.js
@@ -2,9 +2,30 @@ let name = prompt("Lütfen adınızı giriniz:");
 let tckn = Number(prompt("Lütfen TC Kimlik Numaranızı Giriniz."));
 let phoneNumber = Number(prompt("Lütfen Telefon Numaranızı Giriniz:"));
 
-if(name = "" && tckn === 11 && phoneNumber === 10){
-    console.log("Kayıt Başarılı");
+// if(name = "" && tckn === 11 && phoneNumber === 10){
+//     console.log("Kayıt Başarılı");
+// }
+// else{
+//     console.log("Kayıt Başarısız");
+// }
+
+function check(name, tckn, phoneNumber){
+if(name !== ""){
+    if( tckn.lenght === 11){
+        if(phoneNumber.lenght === 10){
+            console.log("Kayıt Başarılı");
+        }
+        else{
+            console.log("Hatalı Telefon Numarası");
+        }
+    }
+    else{
+        console.log("Hatalı TC Kimlik Numarası")
+    }
 }
 else{
-    console.log("Kayıt Başarısız");
+console.log("Hatalı Eksik İsim Girişi")
 }
+}
+
+check(name, tckn, phoneNumber);
