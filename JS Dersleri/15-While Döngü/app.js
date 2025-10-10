e221a96579ed4b925fb0dc3696e8d96130aaed02
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-10 20:14:42 +0300

    Break Contiune

---
+++ JS Dersleri/15-While Döngü/app.js
@@ -0,0 +1,7 @@
+// 1'den 10 a kadar yazdırma
+
+let i=1;
+while(i <=10){
+    console.log(i);
+    i++;
+}
---
+++ JS Dersleri/16- Break Contiune/app.js
@@ -0,0 +1,25 @@
+// break
+
+let sayac = 1;
+debugger;
+while(sayac <= 10){
+     if(sayac==8){
+        break;
+    }
+    console.log(sayac);
+   
+    sayac++;
+}
+
+// contiune
+
+let i = 1;
+while(i<=5){
+   i++;
+     if(sayac == 5){
+        continue;
+    }
+    console.log(i);
+   
+    
+}
