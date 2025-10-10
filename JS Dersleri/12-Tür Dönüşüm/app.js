acd7876da13cefdc276aaced3747e20213aed145
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-10 18:49:06 +0300

    Tür Dönüşümler number-string

--- JS Dersleri/12-Tür Dönüşüm/app.js
+++ JS Dersleri/12-Tür Dönüşüm/app.js
@@ -7,3 +7,13 @@ console.log(a + b);
 let c = 5;
 let d = parseInt("22");
 console.log(c + d);
+
+// Number veri tipinden String veri tipine dönüşüm
+
+let x = String(55);
+console.log(typeof x);
+console.log(x);
+
+let y = (37).toString();
+console.log(y);
+console.log(typeof y);
