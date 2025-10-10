8b7d516b05ba1ce24767509f6c95a6413c63ce75
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-10 19:18:17 +0300

    Update app.js

--- JS Dersleri/14- For Döngüsü/app.js
+++ JS Dersleri/14- For Döngüsü/app.js
@@ -3,3 +3,19 @@
 for(let i=1; i<=10; i++){
     console.log(i);
 }
+
+// 0 dan 10'a kadar çiftler yazma
+
+for(let i=0; i<=10; i+=2){
+    console.log(i);
+}
+
+for(let i=0; i<=10; i++){
+    if(i%2==0){
+        console.log(i  +  " :Ali Kaan");
+    }
+    else{
+        console.log(i + " :Koç");
+    }
+}
+
