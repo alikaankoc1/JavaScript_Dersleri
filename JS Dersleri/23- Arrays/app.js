1f842fdef09ba6a7b60d40451b5e4f431f2804f1
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-11 15:56:17 +0300

    Arrays

---
+++ JS Dersleri/23- Arrays/app.js
@@ -0,0 +1,11 @@
+let mix = [1,2,3,"Ali",5,"Ahmet","AyşeNur",11,19];
+console.log(mix);
+console.log(typeof mix);
+console.log(mix.length);
+console.log(mix[1]);
+console.log(mix[6]);
+
+mix[5] = "ASLAN";
+console.log(mix[5]);
+mix[mix.length-1] = 25;
+console.log(mix[mix.length-1]);
