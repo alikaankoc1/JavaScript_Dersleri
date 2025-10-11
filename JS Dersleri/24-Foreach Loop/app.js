c0be3b6b94a67d7ece0509521863b4ef91995325
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-11 16:07:07 +0300

    Foreach loops

---
+++ JS Dersleri/24-Foreach Loop/app.js
@@ -0,0 +1,13 @@
+// foreach dizilerde kullanılır
+
+let names =["Ali", "Ayşe", "Fatma", "Enes", "Doruk", "Fatih", "Emre"];
+
+names.forEach(function(name){
+    console.log(name);
+    
+})
+
+
+for(let i = 0; i<=names.length-1; i++){
+    console.log(names);
+}
