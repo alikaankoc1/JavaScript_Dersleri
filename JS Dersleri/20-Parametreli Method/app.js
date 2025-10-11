8a5be7f0b88d85366048151a1b355ce4ff19a4ac
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-11 12:54:47 +0300

    Example Method

--- JS Dersleri/20-Parametreli Method/app.js
+++ JS Dersleri/20-Parametreli Method/app.js
@@ -12,3 +12,16 @@ function cube(number){
 cube(5);
 cube(7);
 cube(8);
+
+// yaş sınırına göre function example
+let yas = Number(prompt("Lütfen yaşınızı giriniz:"));
+
+function check(age){
+    if(yas>18){
+        console.log("Ehliyet Alabilirsiniz.");
+    }
+    else{
+        console.log("Ehliyet Alamazsınız.");
+    }
+}
+check(yas);
