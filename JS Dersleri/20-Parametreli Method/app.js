5521b234fbfe63ca46508b929bca5e4008eeb59e
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-11 12:48:26 +0300

    Example Method

--- JS Dersleri/20-Parametreli Method/app.js
+++ JS Dersleri/20-Parametreli Method/app.js
@@ -2,3 +2,13 @@ function yazdir(name,surname){
     console.log(name + " " + surname);
 }
 yazdir("Ali Kaan", "Koç");
+yazdir("Ali", "Veli")
+
+// bir sayının küp alma function
+
+function cube(number){
+    console.log(number*number*number);
+}
+cube(5);
+cube(7);
+cube(8);
