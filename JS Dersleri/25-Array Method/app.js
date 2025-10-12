5168d9c559e53ff903facfe4a828695af75eab38
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-12 13:11:17 +0300

    Arrays unshift method

--- JS Dersleri/25-Array Method/app.js
+++ JS Dersleri/25-Array Method/app.js
@@ -3,3 +3,7 @@ let cars = ["BMW","MERCEDES","VOLVO","HYUNDAİ","TESLA","TOGG"];
 //push method dizinin sonuna eleman ekler.
 cars.push("RENAULT");
 console.log(cars);
+
+//unshift dizinin başına eleman ekler.
+cars.unshift("CHERRY");
+console.log(cars);
