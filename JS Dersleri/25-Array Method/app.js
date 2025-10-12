51f32fb2c9feff9c43d58ae1e2ff6773828fb9d0
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-12 13:23:44 +0300

    Arrays toStirng method

--- JS Dersleri/25-Array Method/app.js
+++ JS Dersleri/25-Array Method/app.js
@@ -19,3 +19,9 @@ console.log(cars);
 //splice alınan parametre değere göre hem silme hem ekleme işlemi yapar.
 cars.splice(2,0, "CHAVROLET");
 console.log(cars);
+
+//toString diziyi stringe çevirir.
+console.log(typeof cars);
+let stringCars = cars.toString();
+console.log(typeof stringCars);
+console.log(stringCars);
