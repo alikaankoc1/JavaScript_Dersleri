492599b0f420a809f4807822939c26ccffa7a13c
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-13 12:26:08 +0300

    Update app.js

--- JS Dersleri/27-Arrays Example-2/app.js
+++ JS Dersleri/27-Arrays Example-2/app.js
@@ -5,3 +5,9 @@ let kitap4 = {isim: "Bir Kadının Yaşamından 24 Saat", yazar: "Stefan Zweig",
 let kitap5 = {isim: "Beyoğlu Rapsodisi", yazar: "Ahmet Ümit", fiyat: 251}
 
 let kitaplar = [kitap1, kitap2, kitap3, kitap4, kitap5];
+
+let raf11 = {kod: "1.1.RAF", goster: false}
+let raf12 = {kod:"1.2.RAF",goster: false};
+let raf13 = {kod: "1.3.RAF",goster:false}
+let raf14 = {kod: "1.4.RAF", goster: false}
+let raf15 = {kod: "1.5.RAF", goster: false}
