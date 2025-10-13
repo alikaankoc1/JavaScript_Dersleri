a1feea0d2f3928cfffb5cc225b7a603c55d97edd
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-13 12:20:34 +0300

    Example arrays

---
+++ JS Dersleri/27-Arrays Example-2/app.js
@@ -0,0 +1,7 @@
+let kitap1 = {isim: "Kirpinin Zarafeti", yazar: "Muriel Barbery", fiyat: 198}
+let kitap2 = {isim: "Gurur ve Önyargı", yazar: "Jane Austen", fiyat: 111}
+let kitap3 = {isim: "Kuyucaklı Yusuf", yazar: "Sabahattin Ali", fiyat: 39}
+let kitap4 = {isim: "Bir Kadının Yaşamından 24 Saat", yazar: "Stefan Zweig", fiyat:44}
+let kitap5 = {isim: "Beyoğlu Rapsodisi", yazar: "Ahmet Ümit", fiyat: 251}
+
+let kitaplar = [kitap1, kitap2, kitap3, kitap4, kitap5];
