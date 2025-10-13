8908cbf1a130c8c2746a89530b80f9445d768a1e
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-13 12:28:24 +0300

    Update app.js

--- JS Dersleri/27-Arrays Example-2/app.js
+++ JS Dersleri/27-Arrays Example-2/app.js
@@ -11,3 +11,31 @@ let raf12 = {kod:"1.2.RAF",goster: false};
 let raf13 = {kod: "1.3.RAF",goster:false}
 let raf14 = {kod: "1.4.RAF", goster: false}
 let raf15 = {kod: "1.5.RAF", goster: false}
+
+
+let raf21 = {kod: "2.1.RAF", goster: false}
+let raf22 = {kod:"2.2.RAF",goster: false};
+let raf23 = {kod: "2.3.RAF",goster:false}
+let raf24 = {kod: "2.4.RAF", goster: false}
+let raf25 = {kod: "2.5.RAF", goster: false}
+
+
+let raf31 = {kod: "3.1.RAF", goster: false}
+let raf32 = {kod:"3.2.RAF",goster: false};
+let raf33 = {kod: "3.3.RAF",goster:false}
+let raf34 = {kod: "3.4.RAF", goster: false}
+let raf35 = {kod: "3.5.RAF", goster: false}
+
+
+let raf41 = {kod: "1.1.RAF", goster: false}
+let raf42 = {kod:"1.2.RAF",goster: false};
+let raf43 = {kod: "1.3.RAF",goster:false}
+let raf44 = {kod: "1.4.RAF", goster: false}
+let raf45 = {kod: "1.5.RAF", goster: false}
+
+
+let raf51 = {kod: "5.1.RAF", goster: false}
+let raf52 = {kod:"5.2.RAF",goster: false};
+let raf53 = {kod: "5.3.RAF",goster:false}
+let raf54 = {kod: "5.4.RAF", goster: false}
+let raf55 = {kod: "5.5.RAF", goster: false}
