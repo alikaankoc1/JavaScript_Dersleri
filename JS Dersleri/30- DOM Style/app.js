f2a4c6f7bebf8a15c03af1e1162cfe10fa818e5e
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-14 14:12:27 +0300

    Example dom style

---
+++ JS Dersleri/30- DOM Style/app.js
@@ -0,0 +1,17 @@
+const list = document.querySelectorAll(".list-group-item ")[1];
+list.style.backgroundColor = "blue";
+list.style.color = "black";
+list.style.fontSize = "25px";
+console.log(list);
+
+
+const button = document.querySelector("#todoClearButton");
+button.innerHTML = "TEMİZLE";
+button.style.fontWeight = "bold";
+button.style.paddingRight = "30px";
+button.style.marginRight = "250px";
+console.log(button);
+
+const title = document.querySelectorAll(".card-title")[0];
+title.style.color = "red";
+console.log(title);
