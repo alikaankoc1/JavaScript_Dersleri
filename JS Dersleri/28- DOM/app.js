ee9075f4220e58e5530097ec1b809455bfe86156
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-14 12:29:17 +0300

    Update app.js

--- JS Dersleri/28- DOM/app.js
+++ JS Dersleri/28- DOM/app.js
@@ -1,9 +1,12 @@
 console.log(document.location);
 // let value = document.location.host;
 // let value = document.location.port;
-let value = document.location.hash;
+// let value = document.location.hash;
+let value = document.links;
+
+let result = document.links.item(3).className;
+console.log(result);
 
 
 
-
-console.log(value);
+// console.log(value);
