5b66ffa8b56a0f882819e369fa7f7ea29ce3e52c
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-14 13:12:43 +0300

    DOM Selector

--- JS Dersleri/29- DOM Selector/app.js
+++ JS Dersleri/29- DOM Selector/app.js
@@ -1,1 +1,22 @@
-// selector 
+// selector: seçiciler 1- ıd 2- className 3- tagName göre seçilir.
+
+let value = document.getElementById("todoAddButton");
+
+console.log(value);
+
+console.log(value.id);
+console.log(value.getAttribute("id"));
+
+console.log(value.className);
+console.log(value.getAttribute("class"));
+
+console.log(value.classList);
+console.log(value.classList[2]);
+
+console.log(value.type);
+console.log(value.getAttribute("type"));
+
+
+// innerHTML HTML elementleri algılar textContent ise algılamaz.
+// value.innerHTML = "<b>SELAM</b>";
+value.textContent = "<b>afsdasd</b>"
