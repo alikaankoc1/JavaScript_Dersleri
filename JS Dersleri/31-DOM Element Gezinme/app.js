abe54a0bae251ca76bd12ef255138c0687e98b1d
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-14 14:52:15 +0300

    Update app.js

--- JS Dersleri/31-DOM Element Gezinme/app.js
+++ JS Dersleri/31-DOM Element Gezinme/app.js
@@ -6,6 +6,11 @@ value = value.children[value.children.length-1];
 value.innerHTML = "ASDSAF"
 value.style.color = "red"
 
+value = Array.from(value.children);
+
+value.forEach(function(form){
+    console.log(form);
+})
 
 
 
