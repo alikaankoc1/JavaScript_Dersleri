dfe3201f6980243a2a5e0720b13a39bf4c3a3db0
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-15 22:48:15 +0300

    update

--- JS Dersleri/32- DOM Element Create/app.js
+++ JS Dersleri/32- DOM Element Create/app.js
@@ -7,7 +7,7 @@ console.log(value)
 const link = document.createElement("a");
 
 link.id = "goToWebSite";
-link.className = "btn btn-dark btn-sm mt-3";
+link.className = "btn btn-dark btn-sm mt-6";
 link.target = "_blank";
 link.href = "https://alikaankoc1.github.io/website";
 link.text = "Web Siteme Git";
