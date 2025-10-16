50c6fa9b12d441a8d803328a4b0bb0d6d5b92aeb
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-16 21:40:47 +0300

    dbclick

--- JS Dersleri/34-Mouse Events/app.js
+++ JS Dersleri/34-Mouse Events/app.js
@@ -14,4 +14,19 @@ window.addEventListener("load",run2);
 function run2(e){
 console.log("load click çalıştı");
 }
-  
+  // click
+const newButton = document.querySelector("#todoAddButton");
+
+newButton.addEventListener("click",run3);
+
+function run3(e){
+    console.log(e.type);
+}
+
+//dbclick
+const cardTitle = document.querySelectorAll(".card-title")[0];
+
+cardTitle.addEventListener("dbclick",run4);
+function run4(e){
+    console.log(e.type);
+}
