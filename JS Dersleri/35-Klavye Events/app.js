19116339730f17d4010f9bf363c9d3bba03c930c
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-16 21:56:36 +0300

    keyup events

--- JS Dersleri/35-Klavye Events/app.js
+++ JS Dersleri/35-Klavye Events/app.js
@@ -15,3 +15,10 @@ document.addEventListener("keydown",run2);
 function run2(e){
     console.log(e.key);
 }
+
+// keyup: tuştan elini kaldırdığında çalışır
+
+document.addEventListener("keyup",run3);
+function run3(e){
+    console.log(e.key);
+}
