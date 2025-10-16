b465d00c50bbd715aebe8d8bcb5726c2c68ac5ed
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-16 22:04:16 +0300

    example events

--- JS Dersleri/35-Klavye Events/app.js
+++ JS Dersleri/35-Klavye Events/app.js
@@ -2,27 +2,37 @@
 
 // keypress : harf ve sayılarda 
 
-document.addEventListener("keypress",run);
+// document.addEventListener("keypress",run);
 
-function run(e){
-    console.log(e.key)
-}
+// function run(e){
+//     console.log(e.key)
+// }
 
 // keydown: tüm klavye karakterlerde 
 
-document.addEventListener("keydown",run2);
+// document.addEventListener("keydown",run2);
 
-function run2(e){
-    console.log(e.keyCode);
-    if(e.keyCode == 116){
-        alert("f5 tuşuna basıldı");
-    }
-    e.preventDefault();
-}
+// function run2(e){
+//     console.log(e.keyCode);
+//     if(e.keyCode == 116){
+//         alert("f5 tuşuna basıldı");
+//     }
+//     e.preventDefault();
+// }
 
 // keyup: tuştan elini kaldırdığında çalışır
 
-document.addEventListener("keyup",run3);
-function run3(e){
-    console.log(e.key);
+// document.addEventListener("keyup",run3);
+// function run3(e){
+//     console.log(e.key);
+// }
+
+// example 
+
+const input = document.querySelector("#todoName");
+
+input.addEventListener("keypress",run);
+
+function run(e){
+    console.log(e.target.value);
 }
