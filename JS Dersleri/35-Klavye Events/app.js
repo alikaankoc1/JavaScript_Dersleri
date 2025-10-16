0177191e8d6775b6f29f8b2381164c7ffee61453
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-16 21:54:06 +0300

    keydown events

--- JS Dersleri/35-Klavye Events/app.js
+++ JS Dersleri/35-Klavye Events/app.js
@@ -1,9 +1,17 @@
 // klavye events
 
-// keypress
+// keypress : harf ve sayılarda 
 
 document.addEventListener("keypress",run);
 
 function run(e){
     console.log(e.key)
+}
+
+// keydown: tüm klavye karakterlerde 
+
+document.addEventListener("keydown",run2);
+
+function run2(e){
+    console.log(e.key);
 }
