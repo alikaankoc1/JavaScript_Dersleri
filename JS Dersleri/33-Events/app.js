ebcfd8489e1bca98ccfee96814cc5e6ac8d7dcc7
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-16 21:21:14 +0300

    Example

--- JS Dersleri/33-Events/app.js
+++ JS Dersleri/33-Events/app.js
@@ -1,9 +1,19 @@
 const newButton = document.querySelector("#todoAddButton");
 console.log(newButton);
 
+const clearButton = document.querySelector("#todoClearButton");
+console.log(clearButton);
+
 newButton.addEventListener("click", changeTitle);
 
 function changeTitle(e){
     document.querySelectorAll(".card-title")[0].textContent = "Click işlemi yapıldı"
+    
+}
+
+clearButton.addEventListener("click",change);
+function change(e){
+    
+    document.querySelectorAll(".list-group-item")[0].style.backgroundColor = "red";
 }
 
