3905cc260dd1e3f3cd1cc524b74dd6b530e64a23
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-17 20:28:36 +0300

    Local Storage

---
+++ JS Dersleri/37-Local Storge/app.js
@@ -0,0 +1,6 @@
+// local storge ile değer eklemek
+
+localStorage.setItem("lesson1", "Math");
+localStorage.setItem("lesson2", "History");
+localStorage.setItem("lesson3", "Scine");
+localStorage.setItem("lesson4", "Physical Education");
