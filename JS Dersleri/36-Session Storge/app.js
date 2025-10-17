da300d49a0cc0a8ea559e9d254f679eb38731397
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-17 20:01:02 +0300

    Session Storge

--- JS Dersleri/36-Session Storge/app.js
+++ JS Dersleri/36-Session Storge/app.js
@@ -2,3 +2,17 @@
 
 sessionStorage.setItem("1","Ali");
 sessionStorage.setItem("2","Kaan");
+sessionStorage.setItem("3","Aslı");
+
+
+// değer silme 
+sessionStorage.removeItem("1");
+// sessionStorage.removeItem("3");
+
+// değer getirme
+let value = sessionStorage.getItem("3");
+if(value == null){
+    console.log("Değer Bulunamadı !");
+}else{
+console.log("Değer Bulundu:" + value);
+}
