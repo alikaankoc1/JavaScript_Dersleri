bcf2f0384110db4d2b1ceab2c5cf760be813e584
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-20 13:59:40 +0300

    Arrow Function

--- JS Dersleri/38-Arrow Function/app.js
+++ JS Dersleri/38-Arrow Function/app.js
@@ -9,3 +9,13 @@ const names = (name,surname) =>{
     console.log(name, surname);
 }
 console.log("Ali Kaan", "Koç");
+
+// sade hal
+const kupAl = x =>  x*x*x;
+console.log("Küp Değer:" +  kupAl(2));
+
+// uzun hal
+const kareAl = (y) =>{
+    return y*y;
+}
+console.log("Kare Değer:" + kareAl(2));
