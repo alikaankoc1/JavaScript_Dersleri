d7be23b31862bc31742386716bfe2b0684cae3ac
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-20 15:04:06 +0300

    For in - of  loops

--- JS Dersleri/41-For in  For/app.js
+++ JS Dersleri/41-For in  For/app.js
@@ -1,5 +1,17 @@
 let names = ["Ahmet","Ali","Sedat"];
 
-names.forEach((name)=>{
-    console.log(name);
-})
+names.forEach((name)=>console.log(name))
+
+// for in: dizinin index numarası döner:  let tanımlama in dizi adı 
+    teams = ["Arsenal","Brighton","Stoke City"];
+for(let team in teams){
+    console.log(team);
+}
+
+// for of: dizinin değeri döner: let tanımlama of dizi adı
+
+citys = ["Manchester","Newcastle","London"];
+
+for(let city of citys){
+    console.log(city);
+}
