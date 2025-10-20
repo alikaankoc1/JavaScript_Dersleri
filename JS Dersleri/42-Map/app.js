1fe6163ce51f4c4e97a6d9e7ce212d8abc2e1f6f
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-20 17:03:17 +0300

    Map kullanimi

---
+++ JS Dersleri/42-Map/app.js
@@ -0,0 +1,12 @@
+const map1 = new Map();
+map1.set(34, "İstanbul");
+map1.set(42, "Konya");
+map1.set(43, "Kütahya");
+map1.set(45, "Manisa");
+
+const donenDeger = map1.get(43);
+console.log(donenDeger);
+
+console.log(map1.size);
+
+
