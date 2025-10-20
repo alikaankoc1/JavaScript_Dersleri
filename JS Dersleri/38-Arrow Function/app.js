f7eeae530a0435df7323fd335672d155d2244b49
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-20 13:49:54 +0300

    Parametreli Arrow Function

--- JS Dersleri/38-Arrow Function/app.js
+++ JS Dersleri/38-Arrow Function/app.js
@@ -1,4 +1,11 @@
+// parametresiz
 const yazdir = ()=>{
     console.log("Merhaba Dünya");
 }
 yazdir();
+
+// parametreli
+const names = (name,surname) =>{
+    console.log(name, surname);
+}
+console.log("Ali Kaan", "Koç");
