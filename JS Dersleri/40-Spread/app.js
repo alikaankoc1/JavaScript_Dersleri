5d937e4dd4a236b016bedc4d1f2ad27e7a1118bc
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-20 14:45:41 +0300

    Example

--- JS Dersleri/40-Spread/app.js
+++ JS Dersleri/40-Spread/app.js
@@ -1,3 +1,9 @@
 let dizi1 = ["Java","Php"];
 let dizi2 = ["Pyhton","C",...dizi1];
 console.log(dizi2);
+
+
+const numbers =  [1,2,3,4,5,6,7,8];
+
+let [a,b,...kalanDizi] = numbers;
+console.log(a,b,kalanDizi);
