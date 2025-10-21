b0882455ffad31ed6968ed0513fcd568c2f4ea05
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-21 13:31:18 +0300

    Class

---
+++ JS Dersleri/Example-Card/MigrosBase.js
@@ -0,0 +1,8 @@
+class MigrosBase{
+    constructor(isim,soysim,kartVarmi,urunler){
+        this.isim = isim;
+        this.soysim = soysim;
+        this.kartVarmi = kartVarmi;
+        this.urunler = urunler;
+    }
+}
