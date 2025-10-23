d7474758c757262e961783e9ea58421d362e5395
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-23 14:03:35 +0300

    exampe promise

--- JS Dersleri/50-PROMİSE/app.js
+++ JS Dersleri/50-PROMİSE/app.js
@@ -26,3 +26,26 @@ createPromise()
 .then((data)=>console.log(data))
 .catch((err)=>console.log(err))
 .finally((final)=> console.log("Her zaman çalışır"))
+
+// example promise-2
+
+
+function readyPerson(url){
+    return new Promise((resolve,reject)=>{
+        const xhr = new XMLHttpRequest();
+        try {
+            xhr.addEventListener("readystatechange",()=>{
+                if(xhr.readyState === 4 && xhr.status === 200){
+                    resolve(JSON.parse(xhr.responseText));
+                }
+            })
+        } catch (error) {
+            reject(error);
+        }
+        xhr.open("GET",url)
+        xhr.send()
+    })
+}
+readyPerson("person.json")
+.then((data)=>console.log(data))
+.catch((err)=>console.log(err))
