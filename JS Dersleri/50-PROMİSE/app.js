5ff090c0cde3e540bab73a9cbb2826d3fa567de5
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-23 13:33:13 +0300

    example promise

--- JS Dersleri/50-PROMİSE/app.js
+++ JS Dersleri/50-PROMİSE/app.js
@@ -1,9 +1,28 @@
 let check = true;
 const promise1 = new Promise((resolve, reject)=>{
     if(check){
-        console.log("Promise başarılı");
+        resolve("Promise başarılı");
     }else{
-        console.log("Promise başarısız");
+        reject("Promise başarısız");
     }
 })
 console.log(promise1);
+
+
+// promise example
+
+const result = false;
+
+function createPromise(){
+    return new Promise((resolve,reject)=>{
+        if(result){
+            resolve("Promise yapısı çalıştı");
+        }else{
+            reject("Promise yapısı başarısız oldu.");
+        }
+    })
+}
+createPromise()
+.then((data)=>console.log(data))
+.catch((err)=>console.log(err))
+.finally((final)=> console.log("Her zaman çalışır"))
