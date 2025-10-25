9f06a5ca058dca9986af6db93d7476ac9063fa9d
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-10-25 12:16:00 +0300

    Update app.js

--- JS Dersleri/51-FetchAPİ/app.js
+++ JS Dersleri/51-FetchAPİ/app.js
@@ -8,3 +8,19 @@ function getPerson(url){
  
 }
 getPerson("person.json");
+
+function getData(url){
+    fetch(url)
+    .then((response)=> response.json())
+    .then((data)=>console.log(data))
+    .then((err)=>console.log(err))
+}
+getPerson("https://jsonplaceholder.typicode.com/todos");
+
+function getPost(url){
+    fetch(url)
+    .then((response)=> response.json())
+    .then((data)=> console.log(data))
+    .then((err)=> console.log(err))
+}
+getPost("https://jsonplaceholder.typicode.com/posts/1/comments");
